edb139caacf5394f2a2a31da1558e036bdc0a77c
thwm-fe <htham5350@gmail.com>
2026-05-27 12:19:06 +0700

    Xoa bot file k can thiet

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 # routers
 from app.routers.order import food_bp
@@ -7,15 +8,24 @@ from app.routers.inventory import inventory_bp
 from app.routers.recipe import recipe_bp
 from app.routers.auth import auth_bp
 from app.routers.admin import admin_bp
+
 app = Flask(__name__)
 
-# register blueprint
-app.register_blueprint(food_bp)
-app.register_blueprint(menu_bp)
-app.register_blueprint(inventory_bp)
-app.register_blueprint(recipe_bp)
-app.register_blueprint(auth_bp)
-app.register_blueprint(admin_bp)
+# CORS
+CORS(app)
+
+# config
+app.config["SECRET_KEY"] = "secret-key-demo"
+
+# register blueprints
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
+app.register_blueprint(admin_bp, url_prefix="/api/admin")
+
+app.register_blueprint(food_bp, url_prefix="/api/order")
+app.register_blueprint(menu_bp, url_prefix="/api/menu")
+app.register_blueprint(inventory_bp, url_prefix="/api/inventory")
+app.register_blueprint(recipe_bp, url_prefix="/api/recipe")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
