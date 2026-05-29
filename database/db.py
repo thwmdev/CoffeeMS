c23d52bdff42529d717a05402c622153ccaaa3ba
khoa19705 <khoa19705@gmail.com>
2026-05-29 17:14:52 +0700

    xóa database thừa

--- database/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="root123",
-        database="quanlyquancafe"
-    )
