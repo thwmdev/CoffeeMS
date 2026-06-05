e928bd0899c49f972dc076be3fc5a26b5cb31f4b
thwm-fe <htham5350@gmail.com>
2026-06-05 10:40:18 +0700

    Add cloud database

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,18 @@
 import mysql.connector
+import os
 
 def get_connection():
+    # Lấy đường dẫn tuyệt đối của file ca.pem nằm ở thư mục gốc dự án
+    base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    ssl_ca_path = os.path.join(base_dir, 'ca.pem')
+
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="quanlyquancafe"
+        host="coffeems-mysql-coffeems.i.aivencloud.com",
+        port=28420,
+        user="avnadmin",
+        password="AVNS_O8rNgn6hXH4ziWooDIT",
+        database="defaultdb",
+        
+        ssl_ca=ssl_ca_path,
+        ssl_disabled=False
     )
